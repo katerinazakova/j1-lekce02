c09aff7dd153f96842510689d531fd20980c2ac2
7ZKaty <zakova.kz@gmail.com>
2023-02-16 15:05:44 +0100

    Domecek a prasatko.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -10,17 +10,52 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
         nakresliCtverec();
-        zofka.turnLeft(45);
+        zofka.penUp();
         zofka.move(150);
         zofka.turnRight(45);
+        zofka.penDown();
+        zofka.move(105);
+        zofka.turnRight(90);
+        zofka.move(105);
+
         zofka.turnRight(45);
+        zofka.penUp();
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(190);
+        zofka.penDown();
+
+
+
+        nakresliCtverec();
+
+        nakresliNozky();
+        zofka.turnLeft(120);
         zofka.move(150);
 
-
+        nakresliNozky();
+        zofka.turnLeft(75);
+        zofka.move(105);
+        zofka.turnRight(90);
+        zofka.move(105);
 
 
         //TODO tady bude kód
 
+
+    }
+
+    private void nakresliNozky() {
+        zofka.turnLeft(120);
+        zofka.move(50);
+        zofka.penUp();
+        zofka.turnLeft(180);
+        zofka.move(50);
+        zofka.penDown();
+        zofka.turnLeft(120);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
 
     }
 
